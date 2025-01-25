f897759aa68d3bf303e6da872c428a55af3b68a9
AUSTIN GODFREY <17agodfrey@gmail.com>
2025-01-24 20:45:22 -0700

    now switches aws logging off on local run

--- back/DateFinder/DateFinder.Api/Configuration/DateFinderServiceCollection.cs
+++ back/DateFinder/DateFinder.Api/Configuration/DateFinderServiceCollection.cs
@@ -29,15 +29,15 @@ namespace DateFinder.Api.Configuration
 
         public void ConfigureServices(IServiceCollection services)
         {
-                        // add AWS lambda hosting 
+            // add AWS lambda hosting 
             services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 
             // Add services to the container.
             services.AddSingleton<IDateFinderConfigurationSettings, DateFinderConfigurationSettings>(); // singleton means single instance throughout the application
             services.AddScoped<IDatesRepository, DatesRepository>(); // scoped - created once per request
 
-            services.AddTransient<IGoogleMapsClient, GoogleMapsClient>(); // transient means a new instance is created every time it is requested
-            //services.AddScoped<IGoogleMapsClient, GoogleMapsClient>();
+            //services.AddTransient<IGoogleMapsClient, GoogleMapsClient>(); // transient means a new instance is created every time it is requested
+            services.AddScoped<IGoogleMapsClient, GoogleMapsClient>();
 
             // Add GoogleMapsClient with logger injection
             //services.AddScoped<IGoogleMapsClient, GoogleMapsClient>(provider =>
@@ -47,10 +47,14 @@ namespace DateFinder.Api.Configuration
             //});
 
             // Add AWS logging to the services
-            services.AddLogging(builder =>
+            // ***IMPORTANT*** this cannot be used if running locally, will cause crash 
+            if (Environment.GetEnvironmentVariable("SOMETHING_ELSE") != null)
             {
-                builder.AddAWSProvider();
-            });
+                services.AddLogging(builder =>
+                {
+                    builder.AddAWSProvider();
+                });
+            }
 
             services.AddTransient<IDateService, DateService>();
 
