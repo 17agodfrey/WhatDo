d46cfaa54d62c6bc384bd2d196f8a3f7ec0869a3
AUSTIN GODFREY <17agodfrey@gmail.com>
2025-06-25 18:05:30 -0600

    changed api url

--- front/src/utils/api.js
+++ front/src/utils/api.js
@@ -1,6 +1,6 @@
 const api = (token) => {
-    const baseUrl = "https://localhost:5001/api/"; 
-    // const baseUrl = "https://zoxazzvkxnn5ubjkmlsfxmhqwa0tazte.lambda-url.us-west-1.on.aws/api/"
+    // const baseUrl = "https://localhost:5001/api/"; 
+    const baseUrl = "https://zoxazzvkxnn5ubjkmlsfxmhqwa0tazte.lambda-url.us-west-1.on.aws/api/"
 
     const headers = {
       "Content-Type": "application/json",
